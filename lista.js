f24b8be5d50cd0b47058a31b3a3f04ccc15a6d99
Giovanni Miranda <giovannimiranda09@gmail.com>
2024-10-10 05:29:54 -0600

    updated

---
+++ lista.js
@@ -0,0 +1,10 @@
+/** js: var 0, let 1, const 2  */
+var nomes = ['maria', 20, 200.5, 'eduardo']
+
+var listaNomes = ['xavier', 'alex', 'carol', 'amanda']
+
+// console.log(nomes)
+var novaLista = listaNomes.sort()
+
+console.log(novaLista)
+
